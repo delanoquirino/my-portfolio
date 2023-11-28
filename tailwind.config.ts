e52287827a6a7b5268619793252fc8cbc5bbccc3
delanoquirino <delanoquirino@gmail.com>
2023-11-28 19:50:47 -0300

    feat: theme listStyle

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,8 +13,13 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      listStyleType: {
+        none: 'none',
+        square: 'square',
+        roman: 'upper-roman',
+      }
     },
   },
-  plugins: [],
+  plugins: [ ],
 }
 export default config
