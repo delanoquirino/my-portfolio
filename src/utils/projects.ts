9493553bbe2c396621a63e2f92eb6c16c5f0a4f1
delanoquirino <delanoquirino@gmail.com>
2023-11-27 21:54:28 -0300

    feat: Add project data

---
+++ src/utils/projects.ts
@@ -0,0 +1,66 @@
+const projects = [
+  {
+    id: 1,
+    title: "Amorim, Moura & martins",
+    description:
+      "Este projeto envolve o desenvolvimento de um site para o escritório de advocacia Amorim, Moura & Martins",
+    imageSrc: "/projects/ammadv1.png",
+    techBadges: [
+      "Next.js",
+      "Styled Components",
+      "Typescript",
+      "Antd",
+      "React Icons",
+      "Swiper",
+    ],
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/delanoquirino/amorimmouramartinsadv",
+    previewUrl: "https://amorimmouramartinsadv.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "Sarah Soares",
+    description: "Projeto desenvolvido para a personal trainer Sarah Soares.",
+    imageSrc: "/projects/sarahsoares.png",
+    techBadges: [
+      "Next.js",
+      "Tailwind",
+      "Typescript",
+      "React Hook Form",
+      "React Icons",
+      "React Scroll",
+    ],
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/delanoquirino/sarah_soares",
+    previewUrl: "https://sarah-soares.vercel.app/",
+  },
+  {
+    id: 3,
+    title: "Landing Page Leadster",
+    description: "Projeto para a vaga de Desenvolvedor Front-end da Leadster",
+    imageSrc: "/projects/leadster.png",
+    techBadges: [
+      "Typescript",
+      "Styled Components",
+      "Next.js",
+      "AntDesign",
+      "React Icons",
+    ],
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/delanoquirino/teste-leadster",
+    previewUrl: "https://teste-leadster-two.vercel.app/",
+  },
+  {
+    id: 4,
+    title: "CineMovie",
+    description:
+      "Projeto consumindo a API TMDB para listagem dos melhores filmes",
+    imageSrc: "/projects/cinemovie.png",
+    techBadges: ["Next.js", "Styled Components", "React Icons", "Fetch"],
+    tag: ["All", "Mobile"],
+    gitUrl: "https://github.com/delanoquirino/CineMovie",
+    previewUrl: "https://cine-movie-rho.vercel.app/",
+  },
+];
+
+export default projects;
