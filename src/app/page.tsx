289cdafcd9fc713a15f25e768c5dea41b783fb39
delanoquirino <delanoquirino@gmail.com>
2023-12-03 01:19:30 -0300

    feat: home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,23 @@
+import { AboutSection } from '@/components/AboutSection'
+import { Footer } from '@/components/Footer'
+import { FormSection } from '@/components/FormSection'
 import { HeroSection } from '@/components/HeroSection'
 import { MySkills } from '@/components/MySkills'
+import { Navbar } from '@/components/Navbar'
+import { ProjectsSection } from '@/components/ProjectsSection'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-black">
-      <div className='container mx-auto px-12 py-4'><HeroSection/></div>
-      <div className='container mx-auto px-12 py-4'><MySkills/></div>
+      <Navbar/>
+      <div className='container mt-24 mx-auto px-12 py-4'>
+        <HeroSection/>
+        <MySkills/>
+        <AboutSection/>
+        <ProjectsSection/>
+        <FormSection/>
+        <Footer/>
+      </div>
     </main>
   )
 }
