519256ccb8375cad6e7404ef21da86a1356985fe
delanoquirino <delanoquirino@gmail.com>
2023-12-03 01:26:42 -0300

    feat: add navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,67 @@
+"use client"
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { NavLink } from "./NavLink";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdOutlineClose } from "react-icons/md";
+import { MenuMobile } from "./MenuMobile";
+import Image from "next/image";
+
+import { usePathname } from "next/navigation";
+import { Transition } from "./Transition";
+
+const navLinks = [
+    {title: "Sobre Mim",
+    path: "#about"},
+    {title: "Projetos",
+    path: "#projects"},
+    {title: "Contato",
+    path: "#contact"},
+    {title: "Blog",
+    path: "blog"},
+]
+
+export const Navbar = () => {
+    const [navbarOpen, setNavbarOpen] = useState(false);
+    const path = usePathname();
+ 
+  return (
+    <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-50 bg-[#121212] bg-opacity-100">
+      <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
+        <Link href={"/"} className="text-2xl md:text-5xl text-white font-semibold">
+        <Image src="/logo.png"  width={50}
+              height={50}
+              alt="logo notbook"/>
+        </Link>
+        <div className="mobile-menu block md:hidden">
+          {!navbarOpen ? (
+            <button
+              onClick={() => setNavbarOpen(true)}
+              className="flex items-center px-3 py-2  text-slate-200 hover:text-cyan-500 hover:border-cyan-500"
+            >
+              <GiHamburgerMenu className="h-5 w-5" />
+            </button>
+          ) : (
+            <button
+              onClick={() => setNavbarOpen(false)}
+              className="flex items-center px-3 py-2  text-slate-200 hover:text-cyan-500 hover:border-cyan-500"
+            >
+              <MdOutlineClose className="h-5 w-5" />
+            </button>
+          )}
+        </div>
+        <div className="menu hidden md:block md:w-auto" id="navbar">
+          <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0" >
+          {path && <Transition />}
+           {navLinks.map((link, index) => (
+                <li key={index}>
+                    <NavLink href={link.path} title={link.title} />
+                   </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      {navbarOpen ? <MenuMobile links={navLinks} /> : null}
+    </nav>
+  );
+};
