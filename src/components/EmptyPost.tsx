210fb46b6168ae63a2bcf531ec2fbb32cf093e65
delanoquirino <delanoquirino@gmail.com>
2023-12-05 21:04:48 -0300

    rafactor: removing unnecessary files

--- src/components/EmptyPost.tsx
+++
@@ -1,7 +0,0 @@
-export const EmptyPost = () => {
-  return (
-    <div className="flex w-full h-screen items-center justify-center">
-        <h1 className="font-bold text-5xl text-sky-500">Sem posts no momento...</h1>
-    </div>
-  )
-}
